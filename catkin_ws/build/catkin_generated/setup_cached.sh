428d6f8fd5c978b28fecd4590c10422e7c9a3161
kylewray <kyle.hollins.wray@gmail.com>
2016-02-21 13:25:12 -0500

    Setup initial catkin workspace

---
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/axiom/Development/sigma_approximation/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/axiom/Development/sigma_approximation/catkin_ws/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/axiom/Development/sigma_approximation/catkin_ws/devel/lib:/home/axiom/Development/sigma_approximation/catkin_ws/devel/lib/x86_64-linux-gnu:/opt/ros/indigo/lib/x86_64-linux-gnu:/opt/ros/indigo/lib:/usr/local/cuda/lib64:/usr/local/cuda/lib:/opt/ros/indigo/lib/python2.7/dist-packages"
+export PATH="/home/axiom/Development/sigma_approximation/catkin_ws/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/axiom/Development/sigma_approximation/catkin_ws/devel/lib/pkgconfig:/home/axiom/Development/sigma_approximation/catkin_ws/devel/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
+export PWD="/home/axiom/Development/sigma_approximation/catkin_ws/build"
+export PYTHONPATH="/home/axiom/Development/sigma_approximation/catkin_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/axiom/Development/sigma_approximation/catkin_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/axiom/Development/sigma_approximation/catkin_ws/src:$ROS_PACKAGE_PATH"
